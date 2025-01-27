b04daaa3917d1076fbd6079153a24e1458ee925e
Honorine684 <honorinegabiam0@gmail.com>
2025-01-27 13:01:59 +0100

    fonction pour calculer la surface de base du  pave droit

--- paveMagique.dart
+++ paveMagique.dart
@@ -9,12 +9,13 @@ void main(){
   double hauteur = double.parse(stdin.readLineSync()!);
 
   double volumePave = calculVolumePave(longueur, largeur, hauteur);
-
   print("Le volume du pavé droit est : $volumePave");
 
   double surfaceTotalePave = calculSurfaceTotalePave(longueur, largeur, hauteur);
-
   print("Le surface totale du pavé droit est : $surfaceTotalePave");
+
+  double surfaceBasePave = calculSurfaceBasePave(longueur, largeur);
+   print("La surface de la base du pavé droit est : $surfaceBasePave");
 }  
 
 // fonction pour le volume du pave droit
@@ -27,3 +28,9 @@ double calculVolumePave(double longueur, double largeur, double hauteur){
 double calculSurfaceTotalePave(double longueur, double largeur, double hauteur){
   return  2*longueur*largeur + 2*longueur*hauteur + 2*largeur*hauteur;
 }
+
+// calcul de la surface de base du pave 
+double  calculSurfaceBasePave(double longueur, double largeur) {
+  return longueur*largeur;
+}
+
