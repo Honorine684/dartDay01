61064662cdd4eff436c98abc645d301c3eefd704
Honorine684 <honorinegabiam0@gmail.com>
2025-01-27 14:32:30 +0100

    fonction pour lancer les des plusieus fois

--- folieDes.dart
+++ folieDes.dart
@@ -1,14 +1,37 @@
 import 'dart:math';
-
+import 'dart:io';
 void main(){
   print("Lancer le dé");
   int nbreAleatoire = lancerDe();
   print("........");
   print("Le nombre obtenu est :  $nbreAleatoire");
+
+  print("Entrez le nombre de fois que vous voulez lancer");
+  int nombreDeLancers = int.parse(stdin.readLineSync()!);
+  print("Voici vos lancers");
+  
+  for(int i=1;i<=nombreDeLancers;i++){
+    print("Le lancer numero $i est de ${lancerDeMultiple(nombreDeLancers)}");
+  }
+  
+ 
+  /*int vosLancers = lancerDeMultiple(nombreDeLancers);
+  print(vosLancers);*/
+
+  
 }
 
 int lancerDe(){
   //int resultat = Random(1,6);
   int  intValue = Random().nextInt(6)+1; 
   return intValue;
+}
+
+int lancerDeMultiple(int nombreDeLancers){
+  int resultat = lancerDe();
+  for(int i=0;i<=nombreDeLancers;){
+    return resultat;
+  }
+  return resultat;
+
 }
